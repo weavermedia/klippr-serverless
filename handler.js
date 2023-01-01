e21fd91fbb0e2293fad1aacd7aba5996678c0d73
weavermedia <danweavermedia@gmail.com>
2023-01-01 13:54:39 -0800

    Initial commit. Lamda function triggered by SQS message.

---
+++ handler.js
@@ -0,0 +1,123 @@
+// const { spawnSync } = require("child_process")
+// const { readFileSync, createWriteStream, unlinkSync } = require("fs")
+// const centra = require('centra')
+// const https = require('https')
+// const AWS = require("aws-sdk")
+
+// const s3 = new AWS.S3()
+
+"use strict"
+
+async function hello(event) {
+  console.log("=== EVENT BODY", event.Records[0].body)
+  if (event.Records[0].body[0] != "{") return
+
+  let body = JSON.parse(event.Records[0].body)
+
+  const { videoId, audioUrl, imageUrl, filterComplex, duration } = body
+
+  if (!videoId || !audioUrl || !imageUrl || !filterComplex || !duration) {
+    console.log("Required params not found")
+    return
+  }
+
+  console.log("=== Video ID", videoId)
+  console.log("=== Audio URL", audioUrl)
+  console.log("=== Image URL", imageUrl)
+  console.log("=== Filter Complex", filterComplex)
+  console.log("=== Duration", duration)
+
+  // const imagePath = "/tmp/image.jpg"
+  // const audioPath = "/tmp/audio.mp3"
+  // const maskPath = "./assets/mask.png"
+
+  // console.log("=== Start downloading files")
+  // await downloadFile(imageUrl, imagePath)
+  // await downloadFile(audioUrl, audioPath)
+  // console.log("=== Finish downloading files")
+
+  // const videoName = "video-" + videoId + ".mp4"
+  // const videoPath = "/tmp/" + videoName
+
+  // console.log("=== Start FFMPEG")
+  // spawnSync(
+  //   "/opt/ffmpeg/ffmpeg",
+  //   [
+  //     "-y",
+  //     "-r", "60",
+  //     "-loop", "1",
+  //     "-i", imagePath,
+  //     "-i", audioPath,
+  //     "-loop", "1",
+  //     "-i", maskPath,
+  //     "-filter_complex", filterComplex,
+  //     "-t", parseInt(duration) - 0.05,
+  //     videoPath
+  //   ],
+  //   { stdio: "inherit" }
+  // )
+  // console.log("=== Finish FFMPEG")
+
+  // const tempFile = readFileSync(videoPath)
+
+  // unlinkSync(imagePath)
+  // unlinkSync(audioPath)
+  // unlinkSync(videoPath)
+
+  // console.log("=== Start S3 upload")
+  // await s3.putObject({
+  //   Bucket: "klippr-temp",
+  //   Key: videoName,
+  //   Body: tempFile
+  // }).promise()
+  // console.log("=== Finish S3 upload")
+
+  // console.log("=== Sending POST request to update Rails video", videoId);
+  // const webhook_url = "https://klippr.video/webhooks/update_video"
+  // const json = {
+  //   video_id: videoId,
+  //   video_temp_url: "https://klippr-temp.s3.us-east-1.amazonaws.com/" + videoName
+  // }
+  // const res = await centra(webhook_url, "POST").body(json, "json").send()
+	// console.log("=== POST result", await res.statusCode)
+
+  // if (res.statusCode == 200) {
+  //   console.log("=== Deleting S3 file")
+  //   await s3.deleteObject({
+  //     Bucket: "klippr-temp",
+  //     Key: videoName
+  //   }).promise()
+  //   console.log("=== Deleted S3 file")
+  // } else {
+  //   console.log("=== S3 FILE NOT DELETED")
+  // }
+
+  // console.log("=== Done with video " + videoId)
+}
+
+// async function downloadFile (url, targetFile) {
+//   return await new Promise((resolve, reject) => {
+//     https.get(url, response => {
+//       const code = response.statusCode ?? 0
+
+//       if (code >= 400) {
+//         return reject(new Error(response.statusMessage))
+//       }
+
+//       if (code > 300 && code < 400 && !!response.headers.location) {
+//         return downloadFile(response.headers.location, targetFile)
+//       }
+
+//       const fileWriter = createWriteStream(targetFile)
+//       .on('finish', () => {
+//         resolve({})
+//       })
+
+//       response.pipe(fileWriter)
+//     }).on('error', error => {
+//       reject(error)
+//     })
+//   })
+// }
+
+module.exports = {hello}
